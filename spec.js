15f73aa429cd0e41a8da7780127919ed04a93716
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-12 00:32:06 -0800

    Added a full integration test to ensure we don't break anything.

--- spec.js
+++ spec.js
@@ -2,12 +2,12 @@ var boggle = require('./boggle.js').boggle;
 var expect = require('chai').expect;
 
 describe("boggle", function () {
-  it("should solve a simple board", function () {
+  it("should find all major word kinds", function () {
     expect(boggle([
       ['e', 'e', 'l'],
       ['m', 'l', 'b'],
       ['t', 'i', 'e'],
-    ])).to.have.members([
+    ])).to.include.members([
       // Horizontal
       'tie',
       'eel',
@@ -22,6 +22,27 @@ describe("boggle", function () {
       'elm',
       'bile',
       'melt'
+
+      // There are more words but this
+      // ensures it catches all the basic
+      // cases
+    ])
+  });
+
+  // A quick integration test to ensure we don't break anything
+  // during refactors.
+  it("should find all words", function () {
+    expect(boggle([
+          ['e', 'e', 'l'],
+          ['m', 'l', 'b'],
+          ['t', 'i', 'e'],
+    ])).to.eql([
+      'eel', 'el', 'elm', 'ell', 'elemi', 'em', 'emit', 'eme', 'lee',
+      'mi', 'mib', 'mil', 'mile', 'milt', 'mill', 'mille', 'me', 'mel',
+      'mell', 'melt', 'lei', 'li', 'lie', 'lit', 'lib', 'libel', 'lime',
+      'bi', 'bit', 'bile', 'bill', 'be', 'bel', 'bell', 'belle', 'bee',
+      'belt', 'belie', 'ti', 'tie', 'time', 'til', 'tile', 'till', 'it',
+      'ill'
     ])
   });
 });
