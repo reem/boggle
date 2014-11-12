ffc81d97b104ff77db1dce8db1c627bb0faf5171
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-11 23:59:13 -0800

    Utility Set type for usage in solution and dictionary.

---
+++ set.js
@@ -0,0 +1,32 @@
+var Set = function (elements) {
+  this._items = toObject(elements);
+  this._len = elements.len;
+};
+
+Set.prototype.contains = function (elem) {
+  return this._items[elem];
+};
+
+Set.prototype.add = function (elem) {
+  this._items[elem] = true;
+  this.len++;
+  return this;
+};
+
+Set.prototype.items = function () {
+  return Object.keys(this._items);
+};
+
+Set.prototype.length = function () {
+  return this._len;
+};
+
+var toObject = function (array) {
+  return array.reduce(function (out, elem) {
+    out[elem] = true;
+    return out;
+  }, {});
+}
+
+module.exports.Set = Set;
+
